424432c800c064eed0dfccc0115779f288991c9b
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-02-13 19:46:43 -0600

    Fix local docker debug script

--- run-docker-container.sh
+++ run-docker-container.sh
@@ -1,5 +1,4 @@
 # used to build + run docker container locally for debugging purposes
-docker build . -t notion-publish-tool --no-cache \ 
-  --build-arg HELLO_MSG=$HELLO_MSG
-
+docker build . -t notion-publish-tool --no-cache \
+    --build-arg HELLO_MSG="$HELLO_MSG"
 docker run -p 3000:3000 notion-publish-tool
--- src/configuration.ts
+++ src/configuration.ts
@@ -2,7 +2,7 @@
 const envFetch = (key: string, default_val?: any) => {
     if (process.env[key]) return process.env[key];
     if (default_val) return default_val;
-    throw new Error(`${key} unavailable`);
+    throw new Error(`Env Var: "${key}" is unavailable`);
 };
 
 export const getPort = (): number => envFetch('PORT', 3000);
