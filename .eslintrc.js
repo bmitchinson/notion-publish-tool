4aaa3fe3e9321eee51107ec577fa632630ff29cb
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-07-04 22:36:32 -0600

    default svelte storybook setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,10 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-    },
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
-        'plugin:prettier/recommended',
-    ],
-    rules: {
-        // "@typescript-eslint/explicit-function-return-type": "off",
-    },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
+  rules: {// "@typescript-eslint/explicit-function-return-type": "off",
+  }
 };
