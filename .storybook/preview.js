5923a5db4e5f2a61b615147cf7397993e646c322
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-07-07 17:38:13 -0600

    add storybook specific styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,12 @@
 import '!style-loader!css-loader!sass-loader!../src/svelte/global.scss';
+import '!style-loader!css-loader!sass-loader!./storybook_preview.scss';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
-}
+};
